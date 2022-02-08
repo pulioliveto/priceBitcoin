3972e4864b1d25a1c5f8690914278b0677a5dbe2
pulioliveto <emilianooliveto02@gmail.com>
2022-02-08 11:21:07 -0300

    uploadind files to repository

---
+++ index.js
@@ -0,0 +1,41 @@
+// Obtenemos los elementos HTML y los guardamos en variables para poder manipularlos en JS
+
+const fechaActual = document.getElementById('fechaActual');
+const contenedorDolar = document.getElementById('contenedor-usd');
+const dolarItem = document.getElementById('dolar');
+const contenedorArs = document.getElementById('contenedor-ars')
+const contenedorBlue = document.getElementById('contenedor-blue')
+
+
+let options = { weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric' };
+
+let fecha = new Date().toLocaleDateString("es-ES", options); 
+
+// Obtenemos la fecha actual y la mandamos a imprimir a la etiqueta <p> del HTML 
+fechaActual.innerHTML = fecha; 
+
+
+// Obtenemos la info de la API 
+fetch('https://api.coindesk.com/v1/bpi/currentprice.json') 
+    .then(response => response.json())
+    .then(data => displayData(data))
+
+
+
+const displayData = (data) => {
+    console.log(data)
+    const USD = Math.round(data.bpi.USD.rate_float);
+    contenedorDolar.textContent = `${USD} USD`
+}
+
+fetch('https://www.dolarsi.com/api/api.php?type=valoresprincipales')
+    .then(response => response.json())
+    .then(valorArs => dataArg(valorArs))
+
+    const dataArg = (valorArs) =>{
+        console.log(valorArs)
+        let ARS = valorArs[0].casa.compra;
+        contenedorArs.textContent = `${ARS} ARS` 
+        let BLUE = valorArs[1].casa.compra;
+        contenedorBlue.textContent = `${BLUE} ARS`
+    }
